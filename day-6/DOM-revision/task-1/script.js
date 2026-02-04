81ddce8470117c5caa4bf9f5a66b55efce720ad3
Likhilesh <likbalpande@example.com>
2026-02-04 11:50:29 +0530

    task-1

--- day-6/DOM-revision/task-1/script.js
+++ day-6/DOM-revision/task-1/script.js
@@ -13,9 +13,42 @@
 */
 }
 
+// const data = {
+//     imgUrl: "...",
+//     name: "Likhilesh",
+//     batch: "CSE-ABCD",
+//     college: "KRMU",
+// };
+
+// const rootElement = document.getElementById("root");
+
+// const imageElement = document.createElement("img");
+// imageElement.src = data.imgUrl;
+
+// rootElement.appendChild(imageElement);
+
+// const newDiv = document.createElement("div");
+// const heading = document.createElement("h2");
+// heading.innerText = data.name;
+// newDiv.appendChild(heading);
+// rootElement.appendChild(newDiv);
+
+// ----------------------------
+
 const data = {
-    imgUrl: "",
+    imgUrl: "...",
     name: "Likhilesh",
     batch: "CSE-ABCD",
     college: "KRMU",
 };
+
+const rootElement = document.getElementById("root");
+
+rootElement.innerHTML = `
+<img src='${data.imgUrl}'>
+<div>
+    <h2>${data.name}</h2>
+    <p>${data.batch}</p>
+    <p>${data.college}</p>
+</div>
+`;
